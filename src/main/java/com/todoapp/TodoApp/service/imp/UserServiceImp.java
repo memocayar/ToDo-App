98f776ff2151fe21fc6ca2e37cfd80a4cd550cec
memocayar <mariaemiliamocayar@gmail.com>
2023-01-05 23:20:17 -0300

    c

--- src/main/java/com/todoapp/TodoApp/service/imp/UserServiceImp.java
+++ src/main/java/com/todoapp/TodoApp/service/imp/UserServiceImp.java
@@ -1,18 +1,1 @@
-package com.todoapp.TodoApp.service.imp;
 
-import com.todoapp.TodoApp.dto.response.UserResponse;
-import com.todoapp.TodoApp.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-@Service
-public class UserServiceImp {
-    @Autowired
-    UserRepository userRepository;
-
-    public List<UserResponse> getAllUsers(){
-        return (List<UserResponse>) userRepository.findAll();
-    }
-}
